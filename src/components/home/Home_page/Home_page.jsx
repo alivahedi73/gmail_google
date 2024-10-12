38a133902b18e677e7dbaeb1ec58fd629ae1d858
Ali.vahedi <ali.bme73@gmail.com>
2024-10-12 23:55:57 +0330

    part_two not complete home page one

--- src/components/home/Home_page/Home_page.jsx
+++ src/components/home/Home_page/Home_page.jsx
@@ -1,6 +1,10 @@
 import "./Home_page.scss";
 import { google_svg } from "./data_home_page";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 const Home_page = () => {
+  let [click,setClick]=React.useState(false)
   return (
     <>
       <div className="home_page_main">
@@ -8,11 +12,43 @@ const Home_page = () => {
           <div className="main_form">
             <div className="google_svg">{google_svg}</div>
             <div className="main_dtail_div">
-                <div className="sign_word_div">
-                    <h1>Sign in</h1>
+              <div className="sign_word_div">
+                <h1>Sign in</h1>
+                <span>to continue to Gmail</span>
+              </div>
+              <div className="input_div">
+                <Box>
+                  <TextField fullWidth label="Email or Phone" id="fullWidth" />
+                </Box>
+                {/* new compents */}
+                <div className="forgat_input">Forgot email?</div>
+                {/* new components */}
+                <div className="deatail__page__home">
+                  <p className="deatail__page__home_p">
+                    Not your computer? Use Guest mode to sign in privately.
+                  </p>
+                  <a href="#" className="deatail__page__home_link">
+                    Learn more about using Guest mode
+                  </a>
                 </div>
-                <div className="input_div"></div>
+              </div>
             </div>
+            <div className="button_home_page_div">
+              <div className="button_home_page_div_create_acu">
+                Create account
+              </div>
+              <div className="button_home_page_div_next">Next</div>
+            </div>
+          </div>
+        </div>
+        <div className="navbar_google">
+          <div className="navbar_google_langugeProcess">
+
+          </div>
+          <div className="navbar_google_Help">
+            <a href="#">Help</a>
+            <a href="#">Privacy</a>
+            <a href="#">Terms</a>
           </div>
         </div>
       </div>
